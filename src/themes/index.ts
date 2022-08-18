07f5be32d8c5cbcc2f454e789e0a8478b41c9977
stksw <kakiage@gmail.com>
2022-08-18 15:28:37 +0900

    impl toPropValue function

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,13 +1,13 @@
 import colors from './colors'
-// import fontSizes from './fontSizes'
-// import letterSpacings from './letterSpacings'
-// import lineHeights from './lineHeights'
+import fontSizes from './fontSizes'
+import letterSpacings from './letterSpacings'
+import lineHeights from './lineHeights'
 import space from './space'
 
 export const theme = {
   space,
-  // fontSizes,
-  // letterSpacings,
-  // lineHeights,
+  fontSizes,
+  letterSpacings,
+  lineHeights,
   colors,
 } as const
---
+++ src/types/style.d.ts
@@ -0,0 +1,129 @@
+export type ResponsiveProp<T> = {
+  base?: T //デフォルト
+  sm?: T // 640px以上
+  md?: T // 768px以上
+  lg?: T // 1024px以上
+  xl?: T // 1280px以上
+}
+
+/**
+ * toPropValue で使用される型を定義。
+ *
+ */
+export type Responsive<T> = T | ResponsiveProp<T>
+
+type SelfPosition =
+  | 'center'
+  | 'end'
+  | 'flex-end'
+  | 'flex-start'
+  | 'self-end'
+  | 'self-start'
+  | 'start'
+
+type ContentPosition = 'center' | 'end' | 'flex-end' | 'flex-start' | 'start'
+
+type ContentDistribution =
+  | 'space-around'
+  | 'space-between'
+  | 'space-evenly'
+  | 'stretch'
+
+type CSSPropertyGlobals =
+  | '-moz-initial'
+  | 'inherit'
+  | 'initial'
+  | 'revert'
+  | 'unset'
+
+export type CSSPropertyAlignItems =
+  | CSSPropertyGlobals
+  | SelfPosition
+  | 'baseline'
+  | 'normal'
+  | 'stretch'
+  // コードの自動補完
+  | (string & {})
+
+export type CSSPropertyAlignContent =
+  | CSSPropertyGlobals
+  | ContentDistribution
+  | 'center'
+  | 'end'
+  | 'flex-end'
+  | 'flex-start'
+  | 'start'
+  | 'baseline'
+  | 'normal'
+  | (string & {})
+
+export type CSSPropertyJustifyItems =
+  | CSSPropertyGlobals
+  | SelfPosition
+  | 'baseline'
+  | 'left'
+  | 'legacy'
+  | 'normal'
+  | 'right'
+  | 'stretch'
+  | (string & {})
+
+export type CSSPropertyJustifyContent =
+  | CSSPropertyGlobals
+  | ContentDistribution
+  | ContentPosition
+  | 'left'
+  | 'normal'
+  | 'right'
+  | (string & {})
+
+export type CSSPropertyFlexWrap =
+  | CSSPropertyGlobals
+  | 'nowrap'
+  | 'wrap'
+  | 'wrap-reverse'
+
+export type CSSPropertyFlexDirection =
+  | CSSPropertyGlobals
+  | 'column'
+  | 'column-reverse'
+  | 'row'
+  | 'row-reverse'
+
+export type CSSPropertyJustifySelf =
+  | CSSPropertyGlobals
+  | SelfPosition
+  | 'auto'
+  | 'baseline'
+  | 'left'
+  | 'normal'
+  | 'right'
+  | 'stretch'
+  | (string & {})
+
+export type CSSPropertyAlignSelf =
+  | CSSPropertyGlobals
+  | SelfPosition
+  | 'auto'
+  | 'baseline'
+  | 'normal'
+  | 'stretch'
+  | (string & {})
+
+type GridLine = 'auto' | (string & {})
+
+export type CSSPropertyGridColumn =
+  | CSSPropertyGlobals
+  | GridLine
+  | (string & {})
+
+export type CSSPropertyGridRow = CSSPropertyGlobals | GridLine | (string & {})
+
+export type CSSPropertyGridAutoFlow =
+  | CSSPropertyGlobals
+  | 'column'
+  | 'dense'
+  | 'row'
+  | (string & {})
+
+export type CSSPropertyGridArea = CSSPropertyGlobals | GridLine | (string & {})
